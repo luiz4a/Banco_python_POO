4ed4b3e5941a5b9847da8c5f35d7692496f226ad
Ma♡ <159453767+luiz4a@users.noreply.github.com>
2024-11-05 11:25:46 +0000

    update

--- app/services/usuario_services.py
+++ app/services/usuario_services.py
@@ -1,27 +1,25 @@
 from models.usuario_models import Usuario
 from repositories.usuario_repositories import UsuarioRepository
 
-
 class UsuarioService:
-    def __init__(self, repository: UsuarioRepository) -> None:
+    def __init__(self, repository: UsuarioRepository):
         self.repository = repository
-    def criar_usuario (self, nome: str, email: str, senha: str):
-        try:
-            usuario = Usuario (nome = nome, email=email, senha=senha)
-            
-            consulta_usuario = self.repository.pesquisar_usuario_por_email(usuario.email)
-                       
-            if consulta_usuario:
-                print("Usuário já Existe no Banco de dados.")
-                return
-            
-            self.repository.salvar_usuario (usuario)
-            print("Usuário salvo com sucesso!")
-        except TypeError as erro:
-            print(f"Erro ao salvar usuário: {erro}")
-        except Exception as erro:
-            print(f"ocorreu um erro inesperado: {erro}")
-
-    def listar_todos_usuarios (self):
-        return self.repository.listar_todos_usuarios()
     
+    def criar_usuario(self, nome: str, email:str, senha: str):
+        try:
+            usuario = Usuario(nome=nome, email=email,senha=senha)
+
+            novo_usuario = self.repository.pesquisar_usuario_por_email(usuario.email)
+
+            if novo_usuario:
+                print("Usuário já cadastrado!")
+
+            self.repository.salvar_usuario(usuario)
+            print("Usuário cadastrado com sucesso!")
+        except TypeError as erro:
+            print(f"Erro ao salvar o usuário: {erro}")
+        except Exception as erro:
+            print(f"Ocorreu um erro inesperado {erro}")
+
+    def listar_todos_usuario(self):
+        return self.repository.listar_usuarios()
